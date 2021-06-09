9277e38e7f91731b2700831a83c8b44df0d4a09f
theerasun_tangngamchit <theerasun@gmail.com>
2021-06-09 12:19:15 +0700

    update sqlite config.

--- database/database.go
+++ database/database.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"database/sql"
 	"gorm.io/gorm/logger"
 	"os"
 	"time"
@@ -24,12 +23,11 @@ func DbOpen(filename string) (*gorm.DB, error) {
 		},
 	)
 
-	db, err := gorm.Open(sqlite.Open(filename+"?_journal_mode=WAL&_synchronous=NORMAL&mode=rwc&cache=shared&_busy_timeout=40000"), &gorm.Config{
+	db, err := gorm.Open(sqlite.Open(filename+"?_journal_mode=WAL&_txlock=exclusive&_synchronous=NORMAL&mode=rwc&cache=shared&_cache_size=10000&_busy_timeout=40000&_loc=UTC"), &gorm.Config{
 		//PrepareStmt: true,
 		SkipDefaultTransaction: true,
 		CreateBatchSize: 10,
 		Logger: newLogger,
-		//Logger: logger.Discard, //disable logger print Slow SQL and happening errors
 	})
 	if err != nil {
 		log.Println(err)
@@ -50,43 +48,13 @@ func DbOpen(filename string) (*gorm.DB, error) {
 
 	sdb.SetMaxIdleConns(1)
 	sdb.SetMaxOpenConns(1)
-	//sdb.SetConnMaxLifetime(time.Nanosecond)
 
-	sqlite3Config(sdb)
+	_, err = sdb.Exec("PRAGMA page_size= 65535;")
+	_, err = sdb.Exec("PRAGMA mmap_size = 30000000000;")
 
-	db.AutoMigrate(&models.People{})
+	err = db.AutoMigrate(&models.People{})
 
-	sdb.Exec("CREATE UNIQUE INDEX IF NOT EXISTS people_id_index ON peoples (id);")
+	_, err = sdb.Exec("CREATE UNIQUE INDEX IF NOT EXISTS people_id_index ON peoples (id);")
 
 	return db, nil
-}
-
-func sqlite3Config(db *sql.DB) {
-
-	db.Exec("PRAGMA page_size= 65535;")
-	db.Exec("PRAGMA cache_size= 8000;")
-	db.Exec("PRAGMA mmap_size = 30000000000;")
-
-	/*
-	db.Exec("PRAGMA journal_mode = WAL;")
-	db.Exec("PRAGMA locking_mode = EXCLUSIVE;")
-	db.Exec("PRAGMA busy_timeout = 20000;")
-	//db.Exec("PRAGMA cache = shared;")
-
-	//db.Exec("PRAGMA cache_spill = ON;")
-	//db.Exec("PRAGMA count_changes = OFF;")
-	db.Exec("PRAGMA encoding = \"UTF-8\";")
-	//db.Exec("PRAGMA journal_mode = delete;")
-	//db.Exec("PRAGMA locking_mode = EXCLUSIVE;")
-	//db.Exec("PRAGMA main.synchronous=NORMAL;")
-	//db.Exec("PRAGMA page_size = 4096;")
-	//db.Exec("PRAGMA shrink_memory;")
-	db.Exec("PRAGMA synchronous = NORMAL;")
-	db.Exec("PRAGMA temp_store = MEMORY;")
-	//db.Exec("legacy_file_format=OFF;")
-
-	//db.Exec("PRAGMA mmap_size=1099511627776;")
-	//db.Exec("PRAGMA threads = 5;")
-	//db.Exec("PRAGMA wal_autocheckpoint = 1638400;")
-	*/
 }
