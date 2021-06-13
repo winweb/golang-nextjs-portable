1a9e2d0c82d77caedc50ce2720c9d5092ac2c452
winweb <theerasun@gmail.com>
2021-06-14 02:13:49 +0700

    add Material UI in foo page.

--- nextjs/pages/foo/index.tsx
+++ nextjs/pages/foo/index.tsx
@@ -2,15 +2,39 @@ import styles from '../../styles/index.module.sass'
 import Link from "next/link";
 import useSWR from "swr";
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
 
-async function fetcher(url: string) {
-    const resp = await fetch(url);
-    return resp.text();
-}
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
 
 const fetcherJson = url => fetch(url).then(r => r.json())
 
+const rows2: GridRowsProp = [
+    { id: 1, name: 'Hello', surname: 'World' },
+    { id: 2, name: 'XGrid', surname: 'is Awesome' },
+    { id: 3, name: 'Material-UI', surname: 'is Amazing' },
+];
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'surname', headerName: 'Surname', width: 150 },
+];
+
 function Foo(): JSX.Element {
+
+    const classes = useStyles();
 
     const registerPeople = async event => {
         event.preventDefault()
@@ -36,11 +60,7 @@ function Foo(): JSX.Element {
         const { data } = useSWR('/all', fetcherJson, { refreshInterval: 10000 })
         console.dir(data)
 
-        return (<div>
-            {data && data.map(todo => (
-                <div key={todo.id}>id: {todo.id} name: {todo.name +" surname: "+ todo.surname}</div>
-            ))}
-        </div>);
+        return data
     }
 
     return (
@@ -53,16 +73,27 @@ function Foo(): JSX.Element {
                 Check out <Link href="/foo/bar">bar</Link>.
             </p>
 
-            <form onSubmit={registerPeople}>
-                <label htmlFor="name">Name</label>
-                <input id="name" name="name" type="text" autoComplete="name" value="some name" required /> <br/>
-                <label htmlFor="surname">Surname</label>
-                <input id="surname" name="surname" type="text" autoComplete="surname" value="some surname" required /> <br/>
-                <button type="submit">Register</button>
+            <form onSubmit={registerPeople} className={classes.root} noValidate autoComplete="off">
+                <TextField id="name" label="Name" defaultValue="some name" autoComplete="name" required /><p/>
+                <TextField id="surname" label="Surname" defaultValue="some surname" autoComplete="surname" required /><p/>
+                <Button variant="contained" color="primary" type="submit">
+                    Register
+                </Button>
             </form>
 
             <h2 className={styles.error}>Last 10 name</h2>
-            <Last10People/>
+            <div style={{ height: 600, width: '100%' }}>
+                <DataGrid rows={Last10People()}
+                          columns={[
+                                    { field: 'id', headerName: 'ID', width: 150 },
+                                    { field: 'name', headerName: 'Name', width: 200 },
+                                    { field: 'surname', headerName: 'Surname', width: 200 },
+                                    { field: 'custom', headerName: 'Custom', width: 300 ,   valueGetter: (params) =>
+                                            `${params.getValue(params.id, 'id') || ''} ${params.getValue(params.id, 'name') || ''}`,},
+                                    ]}
+                          hideFooterPagination={true}
+                          rowHeight={32}/>
+            </div>
         </div>
     );
 }
